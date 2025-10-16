7c19f2f2524216861957b6e516d11c0ee54d9ea5
MounKilian <kilian.moun@ynov.com>
2025-10-16 23:47:23 +0200

    Add Websocket client and html

---
+++ src/client.js
@@ -0,0 +1,18 @@
+const socket = io("https://quizium-server-test.onrender.com");
+
+socket.on("connect", () => {
+    console.log("Connecté au serveur avec ID :", socket.id);
+});
+
+socket.on("disconnect", () => {
+    console.log("Déconnecté du serveur");
+});
+
+function SendEvent() {
+    socket.emit("sendMessage", "Users : ");
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const bouton = document.getElementById('monBouton');
+    bouton.addEventListener('click', SendEvent);
+});
